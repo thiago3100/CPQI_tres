222411181116f1e3ce6572c7fd6861cac82f5bfe
thiago3100 <thfelipe31@gmail.com>
2022-06-14 17:01:03 -0300

    adicionando tratamento de erros

--- src/ExceptionTryCatchFinally.java
+++ src/ExceptionTryCatchFinally.java
@@ -2,27 +2,30 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionTryCatchFinally {
-
-    public static void main(String args[]) {
-        
-        Scanner s = new Scanner(System.in);
-        boolean continua = true;
-        
-        do{
-            try{ 
-                System.out.print("Digite um numero : ");
+    
+    public static void dividir(Scanner s) throws InputMismatchException, ArithmeticException {
+              System.out.print("Digite um numero : ");
                 int a = s.nextInt();
                 
                 System.out.print("Digite um numero : ");
                 int b = s.nextInt();
                 
                 System.out.println(a / b);
+        } 
+    public static void main(String args[]) {
+        
+        
+        Scanner s = new Scanner(System.in);
+        boolean continua = true;
+        
+        
+        do{
+            try{ 
+                dividir(s);
                 continua = false;
-            }catch(InputMismatchException e1){
-                System.err.println("Numeros devem ser inteiros");
-                s.nextLine();
-            }catch(ArithmeticException e2){
-                System.err.println("Divisor deve ser diferente de 0");
+            }catch(InputMismatchException  | ArithmeticException e1){
+                System.err.println("Numero inválido");
+                e1.getMessage();
                 s.nextLine();
             }
             finally{
